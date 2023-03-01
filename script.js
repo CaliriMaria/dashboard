7a8152f1b8722745aa901865a4f7541af3b944dd
mariaCaliri <101271771+mariaCaliri@users.noreply.github.com>
2023-03-01 18:15:45 +0100

    aside

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
 
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
+        let content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
@@ -14,3 +14,37 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+let buttonNotification = document.querySelector('#dropdownNotificationButton');
+let dropdownNotification = document.querySelector('#dropdownNotification');
+
+// const options = {
+//     placement: 'bottom',
+//     triggerType: 'click',
+//     offsetSkidding: 0,
+//     offsetDistance: 10,
+//     delay: 300,
+//     onHide: () => {
+//         console.log('dropdown has been hidden');
+//     },
+//     onShow: () => {
+//         console.log('dropdown has been shown');
+//     },
+//     onToggle: () => {
+//         console.log('dropdown has been toggled');
+//     }
+// };
+
+buttonNotification.addEventListener('click', function (){
+    console.log('cliccato');
+   dropdownNotification.classList.toggle('hidden');
+});
+
+let buttonAvatar = document.querySelector('#dropdownUserAvatarButton');
+let dropdownAvatar = document.querySelector('#dropdownAvatar');
+
+buttonAvatar.addEventListener('click', function (){
+    dropdownAvatar.classList.toggle('hidden');
+})
+
+
